98d7a9987e039c39c361a715749983db26c19558
ifysglbiydgfifs <igbsh43hsdh@gmail.com>
2025-04-08 06:55:47 +0500

    models changed

--- src/models.py
+++ src/models.py
@@ -21,10 +21,8 @@ class User(Base):
     telegram_id = Column(BigInteger, unique=True, index=True, nullable=False)
     username = Column(String, unique=True, nullable=True)
     credits = Column(Integer, default=0)  # Внутренняя валюта
-    
     channels = relationship("Channel", back_populates="user", cascade="all, delete-orphan")
 
-    
 class Entity(Base):
     __tablename__ = "entities"
 
@@ -32,6 +30,7 @@ class Entity(Base):
     name = Column(String, nullable=False)
     type = Column(String, nullable=False)
     cluster_id = Column(Integer, nullable=True)
+    link = Column(String, nullable=True)
 
     news = relationship("News", secondary=news_entity_link, back_populates="entities")
 
@@ -41,14 +40,12 @@ class News(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     text = Column(String, nullable=False)
-    link = Column(String, nullable=False)
+    time = Column(BigInteger, nullable=False)
 
     entities = relationship("Entity", secondary=news_entity_link, back_populates="news")
-    
 
 class UserChannel(Base):
     __tablename__ = "user_channels"
-    
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     channel_url = Column(String, nullable=False)
@@ -56,6 +53,5 @@ class UserChannel(Base):
     user = relationship("User", back_populates="channels")
 
 User.channels = relationship("UserChannel", back_populates="user")
-
 
 Base.metadata.create_all(engine)
